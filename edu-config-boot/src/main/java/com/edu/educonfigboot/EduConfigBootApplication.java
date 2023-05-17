3194342b16f375c5abd467a56c68dae90133d1b6
ice <ibgdn4@gmail.com>
2023-05-17 16:12:50 +0800

    Create configs(edu-config-boot) module.

---
+++ edu-config-boot/src/main/java/com/edu/educonfigboot/EduConfigBootApplication.java
@@ -0,0 +1,13 @@
+package com.edu.educonfigboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduConfigBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduConfigBootApplication.class, args);
+    }
+
+}

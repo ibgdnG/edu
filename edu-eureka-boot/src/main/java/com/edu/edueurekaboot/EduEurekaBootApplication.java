3bc7e8bbbf0f31696d404d8e3f0bfb87790dc3b9
ice <ibgdn4@gmail.com>
2023-05-17 14:34:02 +0800

    Create common utils(edu-common)、dependencies(edu-bom)、eureka register(edu-eureka-boot) modules.

---
+++ edu-eureka-boot/src/main/java/com/edu/edueurekaboot/EduEurekaBootApplication.java
@@ -0,0 +1,13 @@
+package com.edu.edueurekaboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduEurekaBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduEurekaBootApplication.class, args);
+    }
+
+}

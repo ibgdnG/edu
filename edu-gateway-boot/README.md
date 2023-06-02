39da17c71066b4c6cb5f39663b3f64bbcf60c5e1
ice <ibgdn4@gmail.com>
2023-06-02 13:48:19 +0800

    Add gateway server module.

---
+++ edu-gateway-boot/README.md
@@ -0,0 +1,9 @@
+# Getting Started
+
+### startup
+
+run edu-gateway-boot module
+
+then input `http://localhost:9001/boss/ad/space/getAllSpaces` in web browser, will get the page
+
+![img.png](img.png)
---
+++ edu-gateway-boot/src/main/java/org/edu/gateway/EduGatewayBootApplication.java
@@ -0,0 +1,13 @@
+package org.edu.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EduGatewayBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EduGatewayBootApplication.class, args);
+    }
+}
